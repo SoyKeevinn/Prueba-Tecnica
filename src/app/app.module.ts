493823a53e00246381e64a255564c1876429ff40
Kevin <angelo,espinoza@espoch,edu,ec>
2023-01-27 19:08:31 -0500

    estructura

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeardComponent } from './component/heard/heard.component';
+import { BodyComponent } from './component/body/body.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { LayoutSimpleComponent } from './layout/layout-simple/layout-simple.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeardComponent,
+    BodyComponent,
+    FooterComponent,
+    LayoutSimpleComponent
   ],
   imports: [
     BrowserModule,
